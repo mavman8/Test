4d76d98219bd28dd430334467d4fc6d425812084
S. Tang <stmaverick@gmail.com>
2022-06-09 14:10:10 -0400

    revised several files.

--- src/reversePolishNotation.java
+++ src/reversePolishNotation.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class reversePolishNotation {
+import java.util.*;
+
+public class reversePolishNotation {
+
+    private static final Set<String> operators = new HashSet<>((Arrays.asList("")));
+    private int evalPrn(String[] tokens){
+
+        System.out.println("test1");
+        return 0;
+    }
+
 }
--- src/stackCustom.java
+++ src/stackCustom.java
@@ -3,4 +3,35 @@ import java.util.*;
 public class stackCustom {
     public List<Integer> arr;
 
+    public stackCustom() {
+        arr = new ArrayList<>();
+    }
+
+    public void push(int data){
+        arr.add(data);
+    }
+
+    public boolean isEmpty(){
+
+        return arr.isEmpty();
+    }
+
+    public int top(){
+        return arr.get(arr.size() -1);
+    }
+
+    public int pop(){
+        /*if(isEmpty()){
+            return false;
+        }*/
+
+        System.out.println("test");
+        int last = top();
+        arr.remove(arr.size() - 1);
+        return last;
+    }
+
+    public int size(){
+        return arr.size();
+    }
 }
